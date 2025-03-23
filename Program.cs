2b79c038f9d379cd02c7f3603ec5c722716b7caa
wildan <wildannabil101@gmail.com>
2025-03-23 20:41:54 +0700

    git commit -m "implementasi design by contract"

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System;
+using System.Diagnostics;
 
 class SayaTubeVideo
 {
@@ -8,21 +10,30 @@ class SayaTubeVideo
 
     public SayaTubeVideo(string title)
     {
-        if (string.IsNullOrEmpty(title))
-            throw new ArgumentException("Title cannot be null or empty");
+        Debug.Assert(!string.IsNullOrEmpty(title) && title.Length <= 100, "Title cannot be null and must be" +
+            " at most 100 characters long");
 
         Random rand = new Random();
-        this.id = rand.Next(10000, 99999); // Generate random 5-digit ID
+        this.id = rand.Next(10000, 99999);
         this.title = title;
         this.playCount = 0;
     }
 
     public void IncreasePlayCount(int count)
     {
-        if (count < 0)
-            throw new ArgumentException("Play count cannot be negative");
+        Debug.Assert(count > 0 && count <= 10000000, "Play count must be between 1 and 10,000,000");
 
-        this.playCount += count;
+        try
+        {
+            checked
+            {
+                this.playCount += count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Play count exceeded the maximum integer limit.");
+        }
     }
 
     public void PrintVideoDetails()
@@ -42,5 +53,10 @@ class Program
 
         video.IncreasePlayCount(10);
         video.PrintVideoDetails();
+
+        for (int i = 0; i < 100; i++)
+        {
+            video.IncreasePlayCount(10000000);
+        }
     }
 }
